3691602a63cb1981ae2c5904b32432470e56e0e4
Timothy Klenke <Timothy.Klenke@pscl.com>
2024-02-04 22:08:27 -0700

    Work around Resharper error

---
+++ Satori/Utilities/RenderMode.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Satori.Utilities
+{
+    /// <summary>
+    /// Workaround for Resharper error.  This can be reverted when this is fixed in Resharper.
+    /// </summary>
+    /// <remarks>https://youtrack.jetbrains.com/issue/RSRP-494352/Support-rendermode-directive</remarks>
+    public sealed class RenderModeInteractiveServer : RenderModeAttribute
+    {
+        public override IComponentRenderMode Mode => (IComponentRenderMode)RenderMode.InteractiveServer;
+    }
+}
