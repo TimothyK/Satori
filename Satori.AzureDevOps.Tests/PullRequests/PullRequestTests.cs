8f876d5d2372b82a62833372d3f5fec47c6a0b80
Timothy Klenke <Timothy.Klenke@pscl.com>
2024-02-06 23:52:18 -0700

    ✔️AzDO-PR - deserialization tests

--- Satori.AzureDevOps.Tests/PullRequests/PullRequestTests.cs
+++ Satori.AzureDevOps.Tests/PullRequests/PullRequestTests.cs
@@ -41,12 +41,7 @@ namespace Satori.AzureDevOps.Tests.PullRequests
             return srv.GetPullRequestsAsync().Result;
         }
 
-        #endregion Act
-
-        #endregion Helpers
-
-        [TestMethod]
-        public void SmokeTest()
+        private Value SinglePullRequest()
         {
             //Arrange
             SetResponse(GetPullRequestsUrl, PullRequestResponses.SinglePullRequest);
@@ -57,7 +52,67 @@ namespace Satori.AzureDevOps.Tests.PullRequests
             //Assert
             pullRequests.Length.ShouldBe(1);
             var pr = pullRequests.Single();
-            pr.pullRequestId.ShouldBe(710);
+            return pr;
         }
+
+        #endregion Act
+
+        #endregion Helpers
+
+        [TestMethod]
+        public void _SmokeTest() => SinglePullRequest().pullRequestId.ShouldBe(1);
+
+        [TestMethod]
+        public void Title() => SinglePullRequest().title.ShouldBe("My PR Title");
+
+        [TestMethod]
+        public void RepoName() => SinglePullRequest().repository.name.ShouldBe("MyRepoName");
+
+        [TestMethod]
+        public void Project() => SinglePullRequest().repository.project.name.ShouldBe("MyProject");
+
+        [TestMethod]
+        public void IsDraft() => SinglePullRequest().isDraft.ShouldBeFalse();
+
+        [TestMethod]
+        public void MergeCommitMessage() => SinglePullRequest().completionOptions.mergeCommitMessage.ShouldBe("Merged PR 1: Test PR");
+
+        [TestMethod]
+        public void CreationDate() => SinglePullRequest().creationDate
+            .ShouldBe(new DateTime(2023, 10, 11, 6, 32, 15, DateTimeKind.Utc).AddTicks(7700876));
+
+        [TestMethod]
+        public void CreatedByUniqueName() => SinglePullRequest().createdBy.uniqueName.ShouldBe(@"Domain\Timothyk");
+
+        [TestMethod]
+        public void CreatedByDisplayName() => SinglePullRequest().createdBy.displayName.ShouldBe("Timothy Klenke");
+
+        [TestMethod]
+        public void CreatedByImageUrl() => SinglePullRequest().createdBy.imageUrl
+            .ShouldBe("http://devops.test/Team/_api/_common/identityImage?id=c00ef764-dc77-4b32-9a19-590db59f039b");
+
+        [TestMethod]
+        public void Reviewers() => SinglePullRequest().reviewers.Length.ShouldBe(1);
+
+        [TestMethod]
+        public void ReviewerId() => SinglePullRequest().reviewers.Single().id.ShouldBe("c00ef764-dc77-4b32-9a19-590db59f039b");
+
+        [TestMethod]
+        public void ReviewerIsRequired() => SinglePullRequest().reviewers.Single().isRequired.ShouldBeTrue();
+        [TestMethod]
+        public void ReviewerVote() => SinglePullRequest().reviewers.Single().vote.ShouldBe(10);
+        
+        [TestMethod]
+        public void ReviewerUniqueName() => SinglePullRequest().reviewers.Single().uniqueName.ShouldBe(@"Domain\Timothyk");
+
+        [TestMethod]
+        public void ReviewerDisplayName() => SinglePullRequest().reviewers.Single().displayName.ShouldBe("Timothy Klenke");
+
+        [TestMethod]
+        public void ReviewerImageUrl() => SinglePullRequest().reviewers.Single().imageUrl
+            .ShouldBe("http://devops.test/Team/_api/_common/identityImage?id=c00ef764-dc77-4b32-9a19-590db59f039b");
+
+
+
     }
 }
