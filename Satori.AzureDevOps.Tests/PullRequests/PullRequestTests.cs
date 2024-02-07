06cbbc85fa23ff9e1d526437b41abfbead1af9f2
Timothy Klenke <Timothy.Klenke@pscl.com>
2024-02-06 23:17:52 -0700

    ✔️💄AzDO.PR - Perfecting smoke test

--- Satori.AzureDevOps.Tests/PullRequests/PullRequestTests.cs
+++ Satori.AzureDevOps.Tests/PullRequests/PullRequestTests.cs
@@ -1,5 +1,6 @@
 using Flurl;
 using RichardSzalay.MockHttp;
+using Satori.AzureDevOps.Models;
 using Satori.AzureDevOps.Tests.PullRequests.SampleFiles;
 using Shouldly;
 
@@ -8,28 +9,50 @@ namespace Satori.AzureDevOps.Tests.PullRequests
     [TestClass]
     public class PullRequestTests
     {
+        #region Helpers
+
+        #region Arrange
+
         private readonly ConnectionSettings _connectionSettings = new()
         {
             Url = new Uri( "http://devops.test/Team" ), 
             PersonalAccessToken = "test"
         };
 
+        private Url GetPullRequestsUrl =>
+            _connectionSettings.Url
+                .AppendPathSegment("_apis/git/pullrequests")
+                .AppendQueryParam("api-version", "6.0");
+
+        private readonly MockHttpMessageHandler _mockHttp = new();
+
+        private void SetResponse(Url url, byte[] response)
+        {
+            _mockHttp.When(url).Respond("application/json", System.Text.Encoding.Default.GetString(response));
+        }
+
+        #endregion Arrange
+
+        #region Act
+
+        private Value[] GetPullRequests()
+        {
+            var srv = new AzureDevOpsServer(_connectionSettings, _mockHttp.ToHttpClient());
+            return srv.GetPullRequestsAsync().Result;
+        }
+
+        #endregion Act
+
+        #endregion Helpers
+
         [TestMethod]
         public void SmokeTest()
         {
             //Arrange
-            var url = _connectionSettings.Url
-                .AppendPathSegment("_apis/git/pullrequests")
-                .AppendQueryParam("api-version", "6.0");
+            SetResponse(GetPullRequestsUrl, PullRequestResponses.SinglePullRequest);
 
-            var mockHttp = new MockHttpMessageHandler();
-            mockHttp.When(url)
-                .Respond("application/json", System.Text.Encoding.Default.GetString(PullRequestResponses.SinglePullRequest));
-
-            var srv = new AzureDevOpsServer(_connectionSettings, mockHttp.ToHttpClient());
-            
             //Act
-            var pullRequests = srv.GetPullRequestsAsync().Result;
+            var pullRequests = GetPullRequests();
 
             //Assert
             pullRequests.Length.ShouldBe(1);
