386a3b7f0f467158a042e038eaf1f3b5baf23de3
Timothy Klenke <Timothy.Klenke@pscl.com>
2024-02-10 18:47:25 -0700

    💄 Mark some non-null fields as required

--- Satori.AppServices/ViewModels/PullRequests/PullRequest.cs
+++ Satori.AppServices/ViewModels/PullRequests/PullRequest.cs
@@ -4,22 +4,22 @@ namespace Satori.AppServices.ViewModels.PullRequests;
 
 public class PullRequest
 {
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string RepositoryName { get; set; }
-    public string Project { get; set; }  // Project /repository/project/name  {CD, CQ, Shared}
+    public int Id { get; init; }
+    public required string Title { get; init; }
+    public required string RepositoryName { get; init; }
+    public required string Project { get; init; }
     public string Url { get; set; }
-    public Status Status { get; set; }
+    public Status Status { get; init; }
     public bool AutoComplete { get; set; }  //set if /completionOptions/mergeCommitMessage has a value
-    public DateTimeOffset CreationDate { get; set; }
-    public Person CreatedBy { get; set; }
-    public List<Review> Reviews { get; set; }
+    public DateTimeOffset CreationDate { get; init; }
+    public required Person CreatedBy { get; init; }
+    public required List<Review> Reviews { get; init; }
     public List<WorkItem> WorkItems { get; set; }
 
     /// <summary>
     /// PR Tags
     /// </summary>
-    public List<string> Labels { get; set; }
+    public required List<string> Labels { get; init; }
 
     //TODO: Add Comments
 }
