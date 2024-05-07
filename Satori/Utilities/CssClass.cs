32a279960263ac92a07727892919ee898d0a7810
Timothy Klenke <Timothy.Klenke@pscl.com>
2024-05-07 08:21:42 -0600

    💄Sprint Board - clean up typing

---
+++ Satori/Utilities/CssClass.cs
@@ -0,0 +1,42 @@
+﻿namespace Satori.Utilities
+{
+    public class CssClass(string className)
+    {
+        private string ClassName { get; } = className;
+
+        public override string ToString() => ClassName;
+
+        public static CssClass None { get; } = new(string.Empty);
+
+        #region Equality
+
+        private bool Equals(CssClass other)
+        {
+            return ClassName == other.ClassName;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return string.IsNullOrWhiteSpace(ClassName);
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((CssClass)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return ClassName.GetHashCode();
+        }
+
+        public static bool operator ==(CssClass? left, CssClass? right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(CssClass? left, CssClass? right)
+        {
+            return !Equals(left, right);
+        }
+
+        #endregion Equality
+    }
+}
