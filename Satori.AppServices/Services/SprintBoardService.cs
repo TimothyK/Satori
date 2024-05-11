9296675703d230e61f5be8af926144f8a13f2282
Timothy Klenke <Timothy.Klenke@pscl.com>
2024-05-11 00:02:05 -0600

    💄 SprintBoardSrv - minor - vertical alignment

--- Satori.AppServices/Services/SprintBoardService.cs
+++ Satori.AppServices/Services/SprintBoardService.cs
@@ -14,6 +14,8 @@ namespace Satori.AppServices.Services;
 
 public class SprintBoardService(IAzureDevOpsServer azureDevOpsServer, ITimeServer timeServer)
 {
+    #region GetActiveSptringsAsync
+
     public async Task<IEnumerable<Sprint>> GetActiveSprintsAsync()
     {
         var teams = await azureDevOpsServer.GetTeamsAsync();
@@ -22,6 +24,66 @@ public class SprintBoardService(IAzureDevOpsServer azureDevOpsServer, ITimeServe
 
         return iterations.Select(map => ToViewModel(map.Team, map.Iteration));
     }
+
+    private async Task<IEnumerable<(Team Team, Iteration Iteration)>> GetIterationsAsync(IEnumerable<Team> teams)
+    {
+        var iterations = new ConcurrentBag<(Team, Iteration)>();
+        var options = new ParallelOptions() { MaxDegreeOfParallelism = 8 };
+        await Parallel.ForEachAsync(teams, options, async (team, token) =>
+        {
+            if (token.IsCancellationRequested)
+            {
+                return;
+            }
+
+            var iteration = await azureDevOpsServer.GetCurrentIterationAsync(team);
+            if (iteration?.Attributes.FinishDate == null || iteration.Attributes.FinishDate.Value.AddDays(7) <= timeServer.GetUtcNow())
+            {
+                return;
+            }
+
+            iterations.Add((team, iteration));
+        });
+
+        return iterations;
+    }
+
+    private static Sprint ToViewModel(Team team, Iteration iteration)
+    {
+        var azureDevOpsUrl = UriParser.GetAzureDevOpsOrgUrl(team.Url);
+
+        var projectName = team.ProjectName;
+        var teamName = team.Name;
+        var iterationPath = iteration.Path;
+        var sprintBoardUrl = azureDevOpsUrl
+            .AppendPathSegment(projectName)
+            .AppendPathSegment("_sprints/taskBoard")
+            .AppendPathSegment(teamName)
+            .AppendPathSegment(iterationPath.Replace(@"\", "/"));
+
+        var teamID = team.Id;
+        var teamAvatarUrl = azureDevOpsUrl
+            .AppendPathSegment("_api/_common/IdentityImage")
+            .AppendQueryParam("id", teamID);
+
+        return new Sprint()
+        {
+            Id = iteration.Id,
+            Name = iteration.Name,
+            IterationPath = iterationPath,
+            StartTime = iteration.Attributes.StartDate ?? throw new InvalidOperationException($"Iteration {iterationPath} missing startDate"),
+            FinishTime = iteration.Attributes.FinishDate ?? throw new InvalidOperationException($"Iteration {iterationPath} missing finishDate"),
+            TeamId = teamID,
+            TeamName = teamName,
+            ProjectName = team.ProjectName,
+            SprintBoardUrl = sprintBoardUrl,
+            TeamAvatarUrl = teamAvatarUrl,
+        };
+    }
+
+    #endregion GetActiveSptringsAsync
+
+    #region GetWorkItemsAsync
 
     public async Task<IEnumerable<WorkItem>> GetWorkItemsAsync(params Sprint[] sprints)
     {
@@ -92,59 +154,5 @@ public class SprintBoardService(IAzureDevOpsServer azureDevOpsServer, ITimeServe
         return iterationBoardItems.Values.ToList();
     }
 
-    private static Sprint ToViewModel(Team team, Iteration iteration)
-    {
-        var azureDevOpsUrl = UriParser.GetAzureDevOpsOrgUrl(team.Url);
-
-        var projectName = team.ProjectName;
-        var teamName = team.Name;
-        var iterationPath = iteration.Path;
-        var sprintBoardUrl = azureDevOpsUrl
-            .AppendPathSegment(projectName)
-            .AppendPathSegment("_sprints/taskBoard")
-            .AppendPathSegment(teamName)
-            .AppendPathSegment(iterationPath.Replace(@"\", "/"));
-
-        var teamID = team.Id;
-        var teamAvatarUrl = azureDevOpsUrl
-            .AppendPathSegment("_api/_common/IdentityImage")
-            .AppendQueryParam("id", teamID);
-
-        return new Sprint()
-        {
-            Id = iteration.Id,
-            Name = iteration.Name,
-            IterationPath = iterationPath,
-            StartTime = iteration.Attributes.StartDate ?? throw new InvalidOperationException($"Iteration {iterationPath} missing startDate"),
-            FinishTime = iteration.Attributes.FinishDate ?? throw new InvalidOperationException($"Iteration {iterationPath} missing finishDate"),
-            TeamId = teamID,
-            TeamName = teamName,
-            ProjectName = team.ProjectName,
-            SprintBoardUrl = sprintBoardUrl,
-            TeamAvatarUrl = teamAvatarUrl,
-        };
-    }
-
-    private async Task<IEnumerable<(Team Team, Iteration Iteration)>> GetIterationsAsync(IEnumerable<Team> teams)
-    {
-        var iterations = new ConcurrentBag<(Team, Iteration)>();
-        var options = new ParallelOptions() { MaxDegreeOfParallelism = 8 };
-        await Parallel.ForEachAsync(teams, options, async (team, token) =>
-        {
-            if (token.IsCancellationRequested)
-            {
-                return;
-            }
-
-            var iteration = await azureDevOpsServer.GetCurrentIterationAsync(team);
-            if (iteration?.Attributes.FinishDate == null || iteration.Attributes.FinishDate.Value.AddDays(7) <= timeServer.GetUtcNow())
-            {
-                return;
-            }
-
-            iterations.Add((team, iteration));
-        });
-
-        return iterations;
-    }
+    #endregion GetWorkItemsAsync
 }
