1aed61862ef436e3ea24095b50227d3c2bb0c683
Timothy Klenke <timothy@klenkeverse.com>
2024-07-21 23:58:02 -0600

    Home - AzureDevOps config - add form validation

--- Satori.AzureDevOps/ConnectionSettings.cs
+++ Satori.AzureDevOps/ConnectionSettings.cs
@@ -2,6 +2,7 @@
 
 public class ConnectionSettings
 {
+    public bool Enabled { get; init; } = true;
     public required Uri Url { get; init; }
     public required string PersonalAccessToken { get; init; }
 }
