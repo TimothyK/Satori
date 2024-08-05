11e5c4ab76da54723bb524b38dc6e605400364a9
Timothy Klenke <timothy@klenkeverse.com>
2024-08-05 09:19:39 -0600

    ConnectionStringStore - make sync so that it works better with constructors

--- Satori.AzureDevOps/ConnectionSettings.cs
+++ Satori.AzureDevOps/ConnectionSettings.cs
@@ -5,4 +5,11 @@ public class ConnectionSettings
     public bool Enabled { get; init; } = true;
     public required Uri Url { get; init; }
     public required string PersonalAccessToken { get; init; }
+
+    public static ConnectionSettings Default = new()
+    {
+        Enabled = false,
+        Url = new Uri("https://devops.test/Org"),
+        PersonalAccessToken = string.Empty,
+    };
 }
--- Satori/Utilities/ConnectionSettingsStore.cs
+++ Satori/Utilities/ConnectionSettingsStore.cs
@@ -4,13 +4,20 @@ using ConnectionSettings = Satori.AzureDevOps.ConnectionSettings;
 namespace Satori.Utilities
 {
     internal class ConnectionSettingsStore(
-        Blazored.LocalStorage.ILocalStorageService localStorage,
+        Blazored.LocalStorage.ISyncLocalStorageService  localStorage,
         AppServices.Models.ConnectionSettings oldAppSettings
     ) : IConnectionSettingsStore
     {
+        private static class LocalStorageKeys
+        {
+            public const string AzureDevOpsSettings = "AzureDevOpsSettings";
+        }
+
         public ConnectionSettings GetAzureDevOpsSettings()
         {
             return oldAppSettings.AzureDevOps;
+            //return localStorage.GetItem<ConnectionSettings>(LocalStorageKeys.AzureDevOpsSettings)
+            //    ?? ConnectionSettings.Default;
         }
 
         public Kimai.ConnectionSettings GetKimaiSettings()
@@ -25,7 +32,7 @@ namespace Satori.Utilities
 
         public void SetAzureDevOpsSettings(ConnectionSettings settings)
         {
-            throw new NotImplementedException();
+            localStorage.SetItem(LocalStorageKeys.AzureDevOpsSettings, settings);
         }
 
         public void SetKimaiSettings(Kimai.ConnectionSettings settings)
