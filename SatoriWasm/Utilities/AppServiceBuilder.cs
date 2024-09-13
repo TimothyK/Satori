654deed861752c01f7e65ce578a3d47754098289
Timothy Klenke <timothy@klenkeverse.com>
2024-09-12 23:00:00 -0600

    Remove redundant DI registration of HttpClient

--- SatoriWasm/Utilities/AppServiceBuilder.cs
+++ SatoriWasm/Utilities/AppServiceBuilder.cs
@@ -13,8 +13,6 @@ internal static class AppServiceBuilder
     {
         services.AddScoped<IConnectionSettingsStore, ConnectionSettingsStore>();
 
-        services.AddScoped<HttpClient>();
-        
         services.AddScoped<SprintBoardService>();
         services.AddScoped<PullRequestService>();
         services.AddScoped<UserService>();
