37bea370fd65aa7cfbfb9a615b61e99cc7460b21
Timothy Klenke <timothy@klenkeverse.com>
2024-11-16 13:38:11 -0700

    EditWorkItem - Add component

---
+++ Satori/Pages/EditWorkItem.razor.cs
@@ -0,0 +1,20 @@
+﻿using Satori.AppServices.ViewModels.DailyStandUps;
+using Satori.AppServices.ViewModels.WorkItems;
+
+namespace Satori.Pages
+{
+    public partial class EditWorkItem
+    {
+    }
+    public class WorkItemCommentViewModel(WorkItem? workItem, TimeEntry[] timeEntries)
+        : CommentViewModel(
+            CommentType.WorkItem
+            , CommentType.WorkItem.GetComment(timeEntries.FirstOrDefault(entry => entry.Task == workItem))
+            , timeEntries
+            , timeEntries.Where(entry => entry.Task == workItem)
+        )
+    {
+        public WorkItem? WorkItem { get; set; } = workItem;
+    }
+
+}
