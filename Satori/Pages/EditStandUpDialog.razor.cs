70af7b69d320a9fd9add4d9d12c90ff7fa2d4b6d
Timothy Klenke <timothy@klenkeverse.com>
2024-11-17 01:02:45 -0700

    EditStandUp - TimeEntry can only be linked to 1 work item.  Linking Add work item button with linking to a TimeEntry

--- Satori/Pages/EditStandUpDialog.razor.cs
+++ Satori/Pages/EditStandUpDialog.razor.cs
@@ -3,272 +3,294 @@ using Microsoft.AspNetCore.Components;
 using Satori.AppServices.ViewModels.DailyStandUps;
 using Satori.Utilities;
 
-namespace Satori.Pages
+namespace Satori.Pages;
+
+public partial class EditStandUpDialog
 {
-    public partial class EditStandUpDialog
+    private ActivitySummary Activity { get; set; } = null!;
+    private ProjectSummary Project { get; set; } = null!;
+
+    private List<CommentViewModel> Comments { get; set; } = null!;
+
+    protected override void OnParametersSet()
     {
-        private ActivitySummary Activity { get; set; } = null!;
-        private ProjectSummary Project { get; set; } = null!;
-
-        private List<CommentViewModel> Comments { get; set; } = null!;
-
-        protected override void OnParametersSet()
+        if (TimeEntries.Length == 0)
         {
-            if (TimeEntries.Length == 0)
+            return;
+        }
+
+        Activity = TimeEntries.Select(t => t.ParentActivitySummary).Distinct().Single();
+        Project = Activity.ParentProjectSummary;
+
+        Comments = BuildComments();
+
+        base.OnParametersSet();
+    }
+
+    private List<CommentViewModel> BuildComments()
+    {
+        return BuildWorkItemComments()
+            .Concat(BuildTextComments())
+            .ToList();
+    }
+
+    private List<WorkItemCommentViewModel> BuildWorkItemComments()
+    {
+        return TimeEntries
+            .SelectWhereHasValue(entry => entry.Task)
+            .Distinct()
+            .Select(workItem => new WorkItemCommentViewModel(workItem, TimeEntries).With(x => x.WorkItemActivated += OnWorkItemActivated))
+            .ToList();
+    }
+
+    private List<CommentViewModel> BuildTextComments()
+    {
+        var timeEntryComments = TimeEntries.ToDictionary(t => t, _ => new List<(CommentType, string)>());
+
+        foreach (var entry in TimeEntries)
+        {
+            timeEntryComments[entry].AddRange(SplitComment(CommentType.Other, entry));
+            timeEntryComments[entry].AddRange(SplitComment(CommentType.Accomplishment, entry));
+            timeEntryComments[entry].AddRange(SplitComment(CommentType.Impediment, entry));
+            timeEntryComments[entry].AddRange(SplitComment(CommentType.Learning, entry));
+        }
+
+        return timeEntryComments
+            .SelectMany(kvp => kvp.Value.Select(x =>
             {
-                return;
-            }
+                var (type, comment) = x;
+                return new { TimeEntry = kvp.Key, Type = type, Text = comment };
+            }))
+            .GroupBy(
+                map => new { map.Type, map.Text }
+                , map => map.TimeEntry
+                , (key, g) => new CommentViewModel(key.Type, key.Text, TimeEntries, g))
+            .ToList();
+    }
 
-            Activity = TimeEntries.Select(t => t.ParentActivitySummary).Distinct().Single();
-            Project = Activity.ParentProjectSummary;
+    private static IEnumerable<(CommentType, string)> SplitComment(CommentType type, TimeEntry timeEntry)
+    {
+        var value = type.GetComment(timeEntry);
 
-            Comments = BuildComments();
-
-            base.OnParametersSet();
+        if (value == null)
+        {
+            return [];
         }
 
-        private List<CommentViewModel> BuildComments()
-        {
-            return BuildWorkItemComments()
-                .Concat(BuildTextComments())
-                .ToList();
-        }
+        return value.Split('\n')
+            .SelectWhereHasValue(x => string.IsNullOrWhiteSpace(x) ? null : x.Trim())
+            .Select(x => (type, x));
+    }
 
-        private List<WorkItemCommentViewModel> BuildWorkItemComments()
-        {
-            var workItems = TimeEntries.SelectWhereHasValue(entry => entry.Task).Distinct();
-            var workItemComments = workItems
-                .Select(workItem => new WorkItemCommentViewModel(workItem, TimeEntries))
-                .ToList();
-            return workItemComments;
-        }
+    private void AddComment(CommentType type)
+    {
+        var comment = type == CommentType.WorkItem
+            ? new WorkItemCommentViewModel(null, TimeEntries).With(x => x.WorkItemActivated += OnWorkItemActivated)
+            : new CommentViewModel(type, string.Empty, TimeEntries, TimeEntries.Reverse().Take(1));
+        OnWorkItemActivated(comment, EventArgs.Empty);
+        Comments.Add(comment);
+        FocusRequest = comment;
+    }
 
-        private List<CommentViewModel> BuildTextComments()
-        {
-            var timeEntryComments = TimeEntries.ToDictionary(t => t, _ => new List<(CommentType, string)>());
+    private void OnWorkItemActivated(object? sender, EventArgs e)
+    {
+        var workItem = sender as WorkItemCommentViewModel 
+                       ?? throw new ArgumentException($"value should be a {nameof(WorkItemCommentViewModel)}", nameof(sender));
+     
+        var otherWorkItems = Comments
+            .OfType<WorkItemCommentViewModel>()
+            .Where(vm => vm != workItem);
 
+        foreach (var other in otherWorkItems)
+        {
             foreach (var entry in TimeEntries)
             {
-                timeEntryComments[entry].AddRange(SplitComment(CommentType.Other, entry));
-                timeEntryComments[entry].AddRange(SplitComment(CommentType.Accomplishment, entry));
-                timeEntryComments[entry].AddRange(SplitComment(CommentType.Impediment, entry));
-                timeEntryComments[entry].AddRange(SplitComment(CommentType.Learning, entry));
-            }
-
-            return timeEntryComments
-                .SelectMany(kvp => kvp.Value.Select(x =>
+                if (workItem.IsActive[entry] && other.IsActive[entry])
                 {
-                    var (type, comment) = x;
-                    return new { TimeEntry = kvp.Key, Type = type, Text = comment };
-                }))
-                .GroupBy(
-                    map => new { map.Type, map.Text }
-                    , map => map.TimeEntry
-                    , (key, g) => new CommentViewModel(key.Type, key.Text, TimeEntries, g))
-                .ToList();
-        }
-
-        private static IEnumerable<(CommentType, string)> SplitComment(CommentType type, TimeEntry timeEntry)
-        {
-            var value = type.GetComment(timeEntry);
-
-            if (value == null)
-            {
-                return [];
-            }
-
-            return value.Split('\n')
-                .SelectWhereHasValue(x => string.IsNullOrWhiteSpace(x) ? null : x.Trim())
-                .Select(x => (type, x));
-        }
-
-        private void AddComment(CommentType type)
-        {
-            var comment = type == CommentType.WorkItem
-                ? new WorkItemCommentViewModel(null, TimeEntries)
-                : new CommentViewModel(type, string.Empty, TimeEntries, TimeEntries.Reverse().Take(1));
-            FocusRequest = comment;
-            Comments.Add(comment);
-        }
-
-        private CommentViewModel? FocusRequest { get; set; }
-
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (FocusRequest != null)
-            {
-                await FocusRequest.TextBox.FocusAsync();
-                FocusRequest = null;
-            }
-
-            await base.OnAfterRenderAsync(firstRender);
-        }
-    }
-
-    public class CommentViewModel
-    {
-        public CommentViewModel(CommentType type, string? text, IEnumerable<TimeEntry> allTimeEntries,
-            IEnumerable<TimeEntry> activeTimeEntries)
-        {
-            Type = type;
-            Text = text;
-
-            IsActive = allTimeEntries.ToDictionary(t => t, t => (SelectionActiveCssClass)activeTimeEntries.Contains(t));
-        }
-
-        public CommentType Type { get; set; }
-        public string? Text { get; set; }
-
-        public Dictionary<TimeEntry, SelectionActiveCssClass> IsActive { get; private set; }
-
-        public void ToggleActive(TimeEntry timeEntry)
-        {
-            IsActive[timeEntry] = IsActive[timeEntry].Not;
-
-            MarkAsDeleted();
-        }
-
-        private void MarkAsDeleted()
-        {
-            if (IsActive.Values.Any(value => value == SelectionActiveCssClass.Activated))
-            {
-                foreach (var kvp in IsActive.Where(kvp => kvp.Value == SelectionActiveCssClass.ToBeDeleted))
-                {
-                    IsActive[kvp.Key] = SelectionActiveCssClass.Deactivated;
-                }
-            }
-            else
-            {
-                foreach (var kvp in IsActive)
-                {
-                    IsActive[kvp.Key] = SelectionActiveCssClass.ToBeDeleted;
+                    other.ToggleActive(entry);
                 }
             }
         }
-
-        public ElementReference TextBox;
+        StateHasChanged();
     }
 
-    public class SelectionActiveCssClass : CssClass
+    private CommentViewModel? FocusRequest { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-        private SelectionActiveCssClass(string className) : base(className)
+        if (FocusRequest != null)
         {
+            await FocusRequest.TextBox.FocusAsync();
+            FocusRequest = null;
         }
 
-        public static SelectionActiveCssClass Activated { get; } = new("selection-activated");
-        public static SelectionActiveCssClass Deactivated { get; } = new("selection-deactivated");
-        public static SelectionActiveCssClass ToBeDeleted { get; } = new("selection-delete");
+        await base.OnAfterRenderAsync(firstRender);
+    }
+}
 
-        public SelectionActiveCssClass Not => this == Activated ? Deactivated : Activated;
+public class CommentViewModel
+{
+    public CommentViewModel(CommentType type, string? text, IEnumerable<TimeEntry> allTimeEntries,
+        IEnumerable<TimeEntry> activeTimeEntries)
+    {
+        Type = type;
+        Text = text;
 
-        public static implicit operator bool(SelectionActiveCssClass cssClass) => cssClass == Activated;
-        public static implicit operator SelectionActiveCssClass(bool value) => value ? Activated : Deactivated;
+        IsActive = allTimeEntries.ToDictionary(t => t, t => (SelectionActiveCssClass)activeTimeEntries.Contains(t));
     }
 
-    public class CommentType
+    public CommentType Type { get; set; }
+    public string? Text { get; set; }
+
+    public Dictionary<TimeEntry, SelectionActiveCssClass> IsActive { get; private set; }
+
+    public virtual void ToggleActive(TimeEntry timeEntry)
     {
-        private CommentType()
+        IsActive[timeEntry] = IsActive[timeEntry].Not;
+
+        MarkAsDeleted();
+    }
+
+    private void MarkAsDeleted()
+    {
+        if (IsActive.Values.Any(value => value == SelectionActiveCssClass.Activated))
         {
-            _all.Add(this);
-        }
-
-        #region All
-
-        private static readonly List<CommentType> _all = [];
-        public static IEnumerable<CommentType> All() => _all;
-
-        #endregion
-
-        #region Members
-
-        public static readonly CommentType Other = new();
-        public static readonly CommentType Accomplishment = new();
-        public static readonly CommentType Impediment = new();
-        public static readonly CommentType Learning = new();
-        public static readonly CommentType WorkItem = new();
-
-        #endregion
-
-        #region To/From String
-
-        private static readonly Dictionary<CommentType, string> ToStringMap = new()
-        {
-            { Other, nameof(Other) },
-            { Accomplishment, nameof(Accomplishment) },
-            { Impediment, nameof(Impediment) },
-            { Learning, nameof(Learning) },
-            { WorkItem, nameof(WorkItem) }
-        };
-
-        public override string ToString() => ToStringMap[this];
-
-        #endregion
-
-        #region Icon
-
-        private static readonly Dictionary<CommentType, string> IconMap = new()
-        {
-            { Other, "📝" },
-            { Accomplishment, "🏆" },
-            { Impediment, "🧱" },
-            { Learning, "🧠" },
-            { WorkItem, "#" }
-        };
-
-        public string Icon => IconMap[this];
-
-        #endregion Icon
-
-        #region PlaceholderText
-
-        private static readonly Dictionary<CommentType, string> PlaceholderTextMap = new()
-        {
-            { Other, "Describe the subtask worked on" },
-            { Accomplishment, "Achievements, decisions made, documentation added" },
-            { Impediment, "Blockers, either needing help or speed bumps" },
+            foreach (var kvp in IsActive.Where(kvp => kvp.Value == SelectionActiveCssClass.ToBeDeleted))
             {
-                Learning,
-                "Today I Learned.  Like accomplishments but you're smarter.  Share with the team to make them smarter too or help identify training gaps"
-            },
-            { WorkItem, "12345" }
-        };
-
-        public string PlaceholderText => PlaceholderTextMap[this];
-
-        #endregion PlaceholderText
-
-        #region Add Button Label
-
-        private static readonly Dictionary<CommentType, string> AddButtonLabelMap = new()
-        {
-            { Other, "General Comment" },
-            { Accomplishment, "Achievement" },
-            { Impediment, "Impediment" },
-            { Learning, "Today I Learned" },
-            { WorkItem, "Azure DevOps Work Item" }
-        };
-
-        public string AddButtonLabel => AddButtonLabelMap[this];
-
-        #endregion Add Button Label
-
-        #region GetComment
-
-        private static readonly Dictionary<CommentType, Func<TimeEntry, string?>> GetCommentMap = new()
-        {
-            { Other, entry => entry.OtherComments },
-            { Accomplishment, entry => entry.Accomplishments },
-            { Impediment, entry => entry.Impediments },
-            { Learning, entry => entry.Learnings },
-            {
-                WorkItem, entry => entry.Task == null ? null
-                    : entry.Task.Parent == null ? $"D#{entry.Task.Id} {entry.Task.Title}"
-                    : $"D#{entry.Task.Parent.Id} {entry.Task.Parent.Title} » D#{entry.Task.Id} {entry.Task.Title}"
+                IsActive[kvp.Key] = SelectionActiveCssClass.Deactivated;
             }
-        };
-
-        public string? GetComment(TimeEntry? entry) => entry == null ? null : GetCommentMap[this](entry);
-
-        #endregion PlaceholderText
+        }
+        else
+        {
+            foreach (var kvp in IsActive)
+            {
+                IsActive[kvp.Key] = SelectionActiveCssClass.ToBeDeleted;
+            }
+        }
     }
+
+    public ElementReference TextBox;
+}
+
+public class SelectionActiveCssClass : CssClass
+{
+    private SelectionActiveCssClass(string className) : base(className)
+    {
+    }
+
+    public static SelectionActiveCssClass Activated { get; } = new("selection-activated");
+    public static SelectionActiveCssClass Deactivated { get; } = new("selection-deactivated");
+    public static SelectionActiveCssClass ToBeDeleted { get; } = new("selection-delete");
+
+    public SelectionActiveCssClass Not => this == Activated ? Deactivated : Activated;
+
+    public static implicit operator bool(SelectionActiveCssClass cssClass) => cssClass == Activated;
+    public static implicit operator SelectionActiveCssClass(bool value) => value ? Activated : Deactivated;
+}
+
+public class CommentType
+{
+    private CommentType()
+    {
+        _all.Add(this);
+    }
+
+    #region All
+
+    private static readonly List<CommentType> _all = [];
+    public static IEnumerable<CommentType> All() => _all;
+
+    #endregion
+
+    #region Members
+
+    public static readonly CommentType Other = new();
+    public static readonly CommentType Accomplishment = new();
+    public static readonly CommentType Impediment = new();
+    public static readonly CommentType Learning = new();
+    public static readonly CommentType WorkItem = new();
+
+    #endregion
+
+    #region To/From String
+
+    private static readonly Dictionary<CommentType, string> ToStringMap = new()
+    {
+        { Other, nameof(Other) },
+        { Accomplishment, nameof(Accomplishment) },
+        { Impediment, nameof(Impediment) },
+        { Learning, nameof(Learning) },
+        { WorkItem, nameof(WorkItem) }
+    };
+
+    public override string ToString() => ToStringMap[this];
+
+    #endregion
+
+    #region Icon
+
+    private static readonly Dictionary<CommentType, string> IconMap = new()
+    {
+        { Other, "📝" },
+        { Accomplishment, "🏆" },
+        { Impediment, "🧱" },
+        { Learning, "🧠" },
+        { WorkItem, "#" }
+    };
+
+    public string Icon => IconMap[this];
+
+    #endregion Icon
+
+    #region PlaceholderText
+
+    private static readonly Dictionary<CommentType, string> PlaceholderTextMap = new()
+    {
+        { Other, "Describe the subtask worked on" },
+        { Accomplishment, "Achievements, decisions made, documentation added" },
+        { Impediment, "Blockers, either needing help or speed bumps" },
+        {
+            Learning,
+            "Today I Learned.  Like accomplishments but you're smarter.  Share with the team to make them smarter too or help identify training gaps"
+        },
+        { WorkItem, "12345" }
+    };
+
+    public string PlaceholderText => PlaceholderTextMap[this];
+
+    #endregion PlaceholderText
+
+    #region Add Button Label
+
+    private static readonly Dictionary<CommentType, string> AddButtonLabelMap = new()
+    {
+        { Other, "General Comment" },
+        { Accomplishment, "Achievement" },
+        { Impediment, "Impediment" },
+        { Learning, "Today I Learned" },
+        { WorkItem, "Azure DevOps Work Item" }
+    };
+
+    public string AddButtonLabel => AddButtonLabelMap[this];
+
+    #endregion Add Button Label
+
+    #region GetComment
+
+    private static readonly Dictionary<CommentType, Func<TimeEntry, string?>> GetCommentMap = new()
+    {
+        { Other, entry => entry.OtherComments },
+        { Accomplishment, entry => entry.Accomplishments },
+        { Impediment, entry => entry.Impediments },
+        { Learning, entry => entry.Learnings },
+        {
+            WorkItem, entry => entry.Task == null ? null
+                : entry.Task.Parent == null ? $"D#{entry.Task.Id} {entry.Task.Title}"
+                : $"D#{entry.Task.Parent.Id} {entry.Task.Parent.Title} » D#{entry.Task.Id} {entry.Task.Title}"
+        }
+    };
+
+    public string? GetComment(TimeEntry? entry) => entry == null ? null : GetCommentMap[this](entry);
+
+    #endregion PlaceholderText
 }
--- Satori/Pages/EditWorkItem.razor.cs
+++ Satori/Pages/EditWorkItem.razor.cs
@@ -1,20 +1,56 @@
 ﻿using Satori.AppServices.ViewModels.DailyStandUps;
 using Satori.AppServices.ViewModels.WorkItems;
 
-namespace Satori.Pages
+namespace Satori.Pages;
+
+public partial class EditWorkItem
 {
-    public partial class EditWorkItem
+}
+
+public class WorkItemCommentViewModel(WorkItem? workItem, TimeEntry[] timeEntries)
+    : CommentViewModel(
+        CommentType.WorkItem
+        , CommentType.WorkItem.GetComment(timeEntries.FirstOrDefault(entry => entry.Task == workItem))
+        , timeEntries
+        , timeEntries.Where(entry => entry.Task == workItem)
+    )
+{
+    public event EventHandler? WorkItemActivated;
+
+    protected virtual void OnWorkItemActivated()
     {
-    }
-    public class WorkItemCommentViewModel(WorkItem? workItem, TimeEntry[] timeEntries)
-        : CommentViewModel(
-            CommentType.WorkItem
-            , CommentType.WorkItem.GetComment(timeEntries.FirstOrDefault(entry => entry.Task == workItem))
-            , timeEntries
-            , timeEntries.Where(entry => entry.Task == workItem).DefaultIfEmpty(timeEntries.Reverse().First())
-        )
-    {
-        public WorkItem? WorkItem { get; set; } = workItem;
+        WorkItemActivated?.Invoke(this, EventArgs.Empty);
     }
 
+    public string? WorkItemIdToAdd { get; set; }
+    public string? WorkItemIdToAddValidationErrorMessage { get; set; }
+
+    /// <summary>
+    /// The ToggleActive button also serves as the Add button to add a new work item.
+    /// </summary>
+    /// <param name="timeEntry"></param>
+    public override void ToggleActive(TimeEntry timeEntry)
+    {
+        if (WorkItem == null)
+        {
+            if (int.TryParse(WorkItemIdToAdd, out var workItemId) && workItemId > 0)
+            {
+                //TODO: Lookup work item in AzDO
+            }
+            else
+            {
+                WorkItemIdToAddValidationErrorMessage = "Value must be positive integer";
+                return;
+            }
+        }
+
+        base.ToggleActive(timeEntry);
+            
+        if (IsActive[timeEntry])
+        {
+            OnWorkItemActivated();
+        }
+    }
+
+    public WorkItem? WorkItem { get; set; } = workItem;
 }
