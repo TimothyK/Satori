781a41abea9b802b7cd7e4c236a01bb48d67653e
Timothy Klenke <timothy@klenkeverse.com>
2025-01-11 22:28:46 -0700

    💄StandUpEdit - Add TimeEntries property to the CommentViewModel

--- Satori/Pages/StandUp/Components/ViewModels/WorkItemCommentViewModel.cs
+++ Satori/Pages/StandUp/Components/ViewModels/WorkItemCommentViewModel.cs
@@ -59,7 +59,7 @@ public class WorkItemCommentViewModel : CommentViewModel
             _period.Days
                 .Where(day => !day.AllExported)
                 .SelectMany(day => day.TimeEntries)
-                .Except(EntriesUnderEdit)
+                .Except(TimeEntries)
                 .Where(entry => entry.Task?.Id == workItem.Id)
                 .Where(entry => !entry.Exported)
                 .Select(entry => entry.TotalTime)
@@ -121,8 +121,6 @@ public class WorkItemCommentViewModel : CommentViewModel
         }
     }
 
-    private IEnumerable<TimeEntry> EntriesUnderEdit => IsActive.Keys;
-
     /// <summary>
     /// Time against <see cref="WorkItem"/> that is not exported yet and not under edit.
     /// </summary>
@@ -152,7 +150,7 @@ public class WorkItemCommentViewModel : CommentViewModel
     }
 
     public TimeSpan SelectedTime =>
-        EntriesUnderEdit
+        TimeEntries
             .Where(entry => IsActive[entry])
             .Select(entry => entry.TotalTime)
             .Sum();
