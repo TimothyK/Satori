2ee0f96c7e6b200c86b8c7ce6f9ef7cd13bed690
Timothy Klenke <timothy@klenkeverse.com>
2025-04-14 23:43:12 -0600

    SprintBoard - PR - Added link

--- Satori/Pages/SprintBoard/PullRequestView.razor.cs
+++ Satori/Pages/SprintBoard/PullRequestView.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using Satori.AppServices.ViewModels.PullRequests;
 
 namespace Satori.Pages.SprintBoard;
@@ -7,4 +8,9 @@ public partial class PullRequestView
 {
     [Parameter]
     public required PullRequest PullRequest { get; set; }
+
+    private async Task OpenPullRequestAsync(PullRequest pullRequest)
+    {
+        await JsRuntime.InvokeVoidAsync("open", pullRequest.Url, "_blank");
+    }
 }
