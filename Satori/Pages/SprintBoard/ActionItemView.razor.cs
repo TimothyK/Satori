67730f43820c42f577e06b3aadf1921d0b2a5d38
Timothy Klenke <timothy@klenkeverse.com>
2025-05-05 23:35:05 -0600

    SprintBoardUi - Action Item Column added

---
+++ Satori/Pages/SprintBoard/ActionItemView.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using Satori.AppServices.ViewModels.Abstractions;
+
+namespace Satori.Pages.SprintBoard;
+
+public partial class ActionItemView
+{
+    [Parameter]
+    public required ActionItem ActionItem { get; set; }
+}
