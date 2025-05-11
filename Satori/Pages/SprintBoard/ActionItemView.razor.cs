ba40c064b56edf788ff3e76409666ff8daed766c
Timothy Klenke <timothy@klenkeverse.com>
2025-05-10 20:17:20 -0600

    ActionItemView - Open links, other styling

--- Satori/Pages/SprintBoard/ActionItemView.razor.cs
+++ Satori/Pages/SprintBoard/ActionItemView.razor.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using Satori.AppServices.ViewModels.Abstractions;
+using Satori.AppServices.ViewModels.PullRequests;
+using Satori.AppServices.ViewModels.WorkItems;
 
 namespace Satori.Pages.SprintBoard;
 
@@ -7,4 +10,14 @@ public partial class ActionItemView
 {
     [Parameter]
     public required ActionItem ActionItem { get; set; }
+
+    private async Task OpenWorkItemAsync(WorkItem workItem)
+    {
+        await JsRuntime.InvokeVoidAsync("open", workItem.Url, "_blank");
+    }
+
+    private async Task OpenPullRequestAsync(PullRequest pullRequest)
+    {
+        await JsRuntime.InvokeVoidAsync("open", pullRequest.Url, "_blank");
+    }
 }
